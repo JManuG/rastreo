184b6c2e9f6778a5ca3396976974ac869a08c145
marvinmarroquin <marvinmarroquin@gmail.com>
2020-12-30 15:42:25 -0600

    mov1

---
+++ sistema/vista/movform.php
@@ -0,0 +1,297 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Movimientos Corrientes</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Movimientos</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <form action="simple-results.html">
+              <div class="input-group">
+                <input type="search" class="form-control form-control-lg" placeholder="Consulta el env&iacute;o aqu&iacute;">
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-lg btn-default">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<p></p>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+
+        <!-- Timelime example  -->
+        <div class="row">
+          <div class="col-md-6">
+            <!-- The time line -->
+            <div class="timeline">
+              <!-- timeline item -->
+              <div>
+                <i class="fas fa-check bg-green"></i>
+                <div class="timeline-item">
+                  <h3 class="timeline-header"><a href="#">Solicitud Recibida</a> el Miercoles 30 de Diciembre 2020 a las 13:19:00</h3>
+
+                </div>
+              </div>
+              <!-- END timeline item -->
+              <!-- timeline item -->
+              <div>
+                <i class="fas fa-check bg-green"></i>
+                <div class="timeline-item">
+
+                  <h3 class="timeline-header no-border"><a href="#">Ingreso</a>  el Miercoles 30 de Diciembre 2020 a las 13:31:15</h3>
+                </div>
+              </div>
+              <!-- END timeline item -->
+              <!-- timeline item -->
+              <div>
+                <i class="fas fa-circle-thin bg-white"></i>
+                <div class="timeline-item">
+                  <span class="time"><i class="fas fa-clock"></i> 27 mins ago</span>
+                  <h3 class="timeline-header"><a href="#">Salida a Ruta</a>  Pendiente...</h3>
+                </div>
+              </div>
+              <!-- END timeline item -->
+              <!-- timeline time label
+              <div class="time-label">
+                <span class="bg-green">3 Jan. 2014</span>
+              </div> -->
+
+            </div>
+          </div>
+          <div class="col-md-6">
+<!--            <div class="card-orange">-->
+<!--              <img class="img-fluid pad" src="vista/imgs/acuse.jpg" alt="Recolección">-->
+<!---->
+<!--              <p>Imágen de recolección</p>-->
+<!--            </div>-->
+
+            <div class="card-orange">
+              <img class="img-fluid pad" src="vista/imgs/acuse.jpg" alt="Entrega">
+              <p>Imágen de Entrega</p>
+            </div>
+
+          </div>
+          <!-- /.col -->
+        </div>
+      </div>
+      <!-- /.timeline -->
+
+    </section>
+    <!-- /.content -->
+
+
+
+    <div class="row">
+      <div class="col-md-4">
+        <!-- Box Comment -->
+
+    <div class="card card-widget">
+      <div class="card-header">
+          <span class="card-title">Remitente</span>
+        <!-- /.user-block -->
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+          </button>
+        </div>
+        <!-- /.card-tools -->
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body">
+        <span class="description">Solicitud el  Miercoles 30 de Diciembre 2020</span>
+      <!-- /.card-body -->
+
+
+
+    </div>
+    </div>
+
+
+
+
+
+        <div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+        <div class="col-md-4">
+          <!-- Box Comment -->
+
+          <div class="card card-widget">
+            <div class="card-header">
+              <span class="card-title">Destinatario</span>
+              <!-- /.user-block -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Mark as read">
+                  <i class="far fa-circle"></i></button>
+                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                </button>
+              </div>
+              <!-- /.card-tools -->
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <span class="description">información destinatario</span>
+              <!-- /.card-body -->
+
+
+
+            </div>
+          </div>
+          <div>
+
+
+          </div>
+
+        </div>
+
+
+
+      <div class="col-md-4">
+        <!-- Box Comment -->
+
+        <div class="card card-widget">
+          <div class="card-header">
+            <span class="card-title">Envío</span>
+            <!-- /.user-block -->
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Mark as read">
+                <i class="far fa-circle"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+              </button>
+            </div>
+            <!-- /.card-tools -->
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <span class="description">Información del envío</span>
+            <!-- /.card-body -->
+
+
+
+          </div>
+        </div>
+        <div>
+
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+  <!-- /.content-wrapper -->
+
+
+
+
+
+
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <div class="col-md-8">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Coordenadas de fin de proceso</h3>
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body p-0">
+        <div class="d-md-flex">
+          <div class="p-1 flex-fill" style="overflow: hidden">
+            <!-- Map will be created here -->
+            <div id="world-map-markers" style="height: 325px; overflow: hidden">
+              <div class="map">
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36735.391016478556!2d-90.5269779011658!3d14.58012972001122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a3ec427650ff%3A0xc4e8a64136d7a539!2sZone%2014%2C%20Guatemala%20City%2C%20Guatemala!5e0!3m2!1sen!2ssv!4v1609364277988!5m2!1sen!2ssv" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <div class="card-pane-right bg-navy pt-2 pb-2 pl-4 pr-4">
+            <div class="description-block mb-4">
+              <div class="sparkbar pad" data-color="#fff">latitud</div>
+              <h5 class="description-header"></h5>
+              <span class="description-text">Latitud</span>
+            </div>
+            <!-- /.description-block -->
+            <div class="description-block mb-4">
+              <div class="sparkbar pad" data-color="#fff">longitud</div>
+              <h5 class="description-header"></h5>
+              <span class="description-text">Longitud</span>
+            </div>
+            <!-- /.description-block -->
+            <div class="description-block">
+              <div class="sparkbar pad" data-color="#fff">Altura</div>
+              <h5 class="description-header"></h5>
+              <span class="description-text">Altura</span>
+            </div>
+            <!-- /.description-block -->
+          </div><!-- /.card-pane-right -->
+        </div><!-- /.d-md-flex -->
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+
+  </div></div>
