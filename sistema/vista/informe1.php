52c0b15666a8a70fa2af99223d8ee0ae1a15b24f
marvinmarroquin <marvinmarroquin@gmail.com>
2021-01-06 17:25:23 -0600

    informe1.1

--- sistema/vista/informe1.php
+++ sistema/vista/informe1.php
@@ -4,10 +4,10 @@
   <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>ChartJS</h1>
+        <div class="col-sm-12">
+          <h1>Estad&iacute;stica de servicio</h1>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-12">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active">ChartJS</li>
@@ -21,9 +21,9 @@
   <section class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-10">
          <!-- BAR CHART -->
-          <div class="card card-success">
+          <div class="card card-navy">
             <div class="card-header">
               <h3 class="card-title">Estad&iacute;stica de env&iacute;os</h3>
 
