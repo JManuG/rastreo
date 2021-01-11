4226e55a47f90c0f4159e6962a54f50c5447e57f
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-10 22:24:49 -0600

    Update test_db.php

--- class/test_db.php
+++ class/test_db.php
@@ -2,15 +2,15 @@
 
 ini_set ("display_errors","1" );
 error_reporting(E_ALL);
-
+echo "1;";
 /*Incluimos el fichero de la clase*/
 require 'db.php';
- 
+ echo "2";
 //$mi = new Db;
  
 /*Creamos la instancia del objeto. Ya estamos conectados*/
 $bd=Db::getInstance();
- 
+ echo "3";
  /*Creamos una consulta sencilla de los CHK*/
 $sql='SELECT cod_chk FROM chk';
  
