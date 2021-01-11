bf8112a6c235f7a06df7c8e859afe90ed0bb4636
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-11 07:16:32 -0600

    Update test_db.php

--- class/test_db.php
+++ class/test_db.php
@@ -12,7 +12,31 @@ require 'db.php';
 $bd=Db::getInstance();
  echo "3";
 
-$sql_1="SELECT * FROM usuario";
+$sql_1="SELECT 
+                a.id_usr, 
+                a.usr_cod, 
+                a.usr_pass, 
+                a.usr_nombre, 
+                a.cli_codigo, 
+                a.id_grupo, 
+                a.nivel, 
+                a.depto, 
+                a.id_ccosto, 
+                a.area, 
+                a.producto, 
+                a.posicion, 
+                a.estado, 
+                a.dias_vencimiento, 
+                c.cli_nombre, 
+                c.cli_direccion,
+                cc.ccosto_codigo, 
+                cc.ccosto_nombre, 
+                cc.ccosto_telefono 
+            FROM usuario a 
+            INNER JOIN cliente c 
+            ON a.cli_codigo=c.cli_id
+            INNER JOIN centro_costo cc 
+            ON a.id_ccosto=cc.id_ccosto";
  
 $stmt=$bd->consultar($sql_1);
   echo "4";
