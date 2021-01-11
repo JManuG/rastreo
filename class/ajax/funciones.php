0b16e2111ea5dcd01221f2b44579d400cda3b2af
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-10 22:05:17 -0600

    Update funciones.php

--- class/ajax/funciones.php
+++ class/ajax/funciones.php
@@ -1,5 +1,7 @@
 <?php
 function valida_usuario($form){
+    ini_set ("display_errors","1" );
+error_reporting(E_ALL);
     $respuesta = new xajaxResponse();
     require 'class/db.php';
     $usr	=$form['usr'];
