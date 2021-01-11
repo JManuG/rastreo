78f4239ef527cbaeb2a63687408ce262ad1bd7c0
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-10 22:47:00 -0600

    Update db.php

--- class/db.php
+++ class/db.php
@@ -8,10 +8,10 @@
  *
  **/
 
-//ini_set ("display_errors","1" );
-//error_reporting(E_ALL);
+ini_set ("display_errors","1" );
+error_reporting(E_ALL);
 
-date_default_timezone_set('America/El_Salvador');
+//date_default_timezone_set('America/El_Salvador');
 class Db{
 	//Declarando los parametros de conexion todo privado para mayor control....
 	private $servidor='rastreo.mysql.database.azure.com';
