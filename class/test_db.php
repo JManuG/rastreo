d0eb38b41c13a0d0ef22fd9e4f4d3a3f97669fed
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-10 21:38:34 -0600

    Update test_db.php

--- class/test_db.php
+++ class/test_db.php
@@ -1,36 +1,25 @@
 <?php
+$servername = "rastreo.mysql.database.azure.com";
+$username = "root2@rastreo";
+$password = "1v341F1ca";
 
-ini_set ("display_errors","1" );
-error_reporting(E_ALL);
-
-/*Incluimos el fichero de la clase*/
-require 'db.php';
- 
-//$mi = new Db;
- 
-/*Creamos la instancia del objeto. Ya estamos conectados*/
-$bd=Db::getInstance();
- 
- /*Creamos una consulta sencilla de los CHK*/
-$sql='SELECT cod_chk FROM chk';
- 
-/*Ejecutamos la consulta*/
-$stmt=$bd->consultar($sql);
-
-
- 
-/*Realizamos un bucle para ir obteniendo los resultados*/
-while ($row=$bd->obtener_fila($stmt,0)){
-	echo $row['cod_chk'].'<br />';
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=rastreo", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 }
-
-$sql_1="SELECT cod_usuario FROM usuario";
- 
-$stmt=$bd->consultar($sql_1);
- 
-/*Realizamos un bucle para ir obteniendo los resultados*/
-while ($row=$bd->obtener_fila($stmt,0)){
-	echo $row['cod_usuario'].'<br />';
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
 }
+?>
 
+<?php
+function getFruit($conn) {
+    $sql = 'SELECT * FROM usuarios ORDER BY 1';
+    foreach ($conn->query($sql) as $row) {
+        print_r($row);
+    }
+}
 ?>
