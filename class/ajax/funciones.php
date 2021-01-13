28b112250eaf6c2e533f1ccbbb83dd9035b51b16
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-13 11:48:38 -0600

    Update funciones.php

--- class/ajax/funciones.php
+++ class/ajax/funciones.php
@@ -1,14 +1,8 @@
 <?php
 function valida_usuario($form){
     $respuesta = new xajaxResponse();
-
-
-            //session_unset();   // Eliminando Variables desesion.
-            //session_destroy(); // Terminando sesion creadaantes.
             $error_form="Usuario no Valido";
             $respuesta->assign("mensaje","innerHTML","<span class='textorojo_bold'>".$error_form."</span>");
-        //}
         return $respuesta;
-    //}
 }
 ?>
