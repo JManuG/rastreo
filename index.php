2a3fb8ea36c4acccabcf9a5d3cce2aa58730870f
marvinmarroquin <55768169+marvinmarroquin@users.noreply.github.com>
2021-01-14 05:05:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-//ini_set ("display_errors","1" );
-//error_reporting(E_ALL);
+ini_set ("display_errors","1" );
+error_reporting(E_ALL);
 //echo "1";
 require_once ("lib/ajax/xajax_core/xajax.inc.php");
 require_once ("class/ajax/funciones.php");
