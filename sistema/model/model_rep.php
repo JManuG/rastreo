0f7bddafb4ad14d43febd319a2a60be153edfb0a
marvinmarroquin <marvinmarroquin@gmail.com>
2021-01-19 17:22:40 -0600

    Cambios informe1.3

--- sistema/model/model_rep.php
+++ sistema/model/model_rep.php
@@ -66,6 +66,7 @@ class model_rep extends Db
     {
 
 
+
         $db=Db::getInstance();
 
         $sql_c = "SELECT m.id_chk,c.chk_descripcion, count(m.id_movimiento) as cantidad FROM `movimiento` m inner join chk_id c on m.id_chk=c.id_chk where m.fecha_date between '$date1' and '$date2' group by 1,2 ";
