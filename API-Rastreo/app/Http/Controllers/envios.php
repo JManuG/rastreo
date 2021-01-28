432973e32b61cd015c56bd87e6d7f44247135d5c
eescobar <edwin.escobar@itca.edu.sv>
2021-01-28 14:24:18 -0600

    apirest laravel 1

---
+++ API-Rastreo/app/Http/Controllers/envios.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//estos dos comandos se utilizan para poder acceder a base de datos
+//el segundo es para la respuesta
+use DB;
+use Response;
+
+class envios extends Controller
+{
+    //
+}
