bea8100bf8410dcb9036695c4fe8e995c7e1db9e
marvinmarroquin <marvinmarroquin@gmail.com>
2021-01-27 20:16:10 -0600

    Cambios login3

--- public/api/cestado.php
+++ public/api/cestado.php
@@ -1,18 +1,15 @@
 <?php
 
-//ini_set ("display_errors","0" );
-//error_reporting(E_ALL);
-
         $a=array(
             'id' => 1001,
             'nombre' => 'Marvin',
             'apellido' => 'Abrego',
             'telefono' => '75489856',
-            'correo' => 'mabrego@enia.com.gt',
+            'correo' => 'm.abrego@enia.com.gt',
             'comercio_id' => 1,
             'foto' => 'null',
-            'token' => '8fdsf1g885dfgg6489g7f8886448fgggf4fgffg886'
+            'token' => '8fdsf1g885dfgg6489g7f8886448fgggf4fgffg886',
 
         );
-    echo json_encode($a);
+    return json_decode($a);
 ?>
