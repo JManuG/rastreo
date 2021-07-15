1ae365f5062b8962357214e2b7b1af5edf95d7c9
Inghtd <edwin.escobar@itca.edu.sv>
2021-07-15 08:02:37 -0600

    modificacion de horarios y Nuevo reporte de Excel 1.3

--- sistema/vista/excel_masivo.php
+++ sistema/vista/excel_masivo.php
@@ -12,6 +12,16 @@ $nivel=$_SESSION['nivel'];
 
 $x1= new historico_ingresos();
 
+$filename="Reporte_general".time().".xls";
+           
+            header("Accept-Ranges:bytes");
+            header("Content-type:application/vnd.ms-excel");
+            header("Content-Disposition:attachment;filename=".$filename.".xls");
+            header("Pragma: no-cache");
+            header("Expires: 0");
+            header("Pragma: no-cache");
+
+            
 $x1->rep_historico_full($f1,$f2);
 
 ?>
